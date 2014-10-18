0e8999324d28549d0fbf2fc8091bdb0d4823f59f
Juha-Matti Santala <juhis@chartio.com>
2014-10-18 14:15:02 -0700

    Remove test code

--- background.js
+++ background.js
@@ -2,6 +2,5 @@ chrome.browserAction.onClicked.addListener(function(activeTab) {
     console.log('Execute');
     chrome.tabs.executeScript({
         file: "hilight.js"
-        // code: 'document.body.style.backgroundColor="red"'
     });
 });
