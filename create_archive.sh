a98058b3226f05379422ff533f7b10674af5e3df
Raimo Tuisku <raimo.tuisku@gmail.com>
2014-10-19 18:55:00 -0700

    Update create_archive.sh

--- create_archive.sh
+++ create_archive.sh
@@ -1,7 +1,6 @@
-cp *.json bin/
-cp *.html bin/
-cp *.png bin/
-cp *.css bin/
+mkdir -p bin
+rm -f bin/*
+cp *.json *.html *.png *.css *.js bin/
 echo "Copied files over to bin/"
 zip -r tsupextension.zip bin/
 echo "Created zip archive tsupextension.zip"
